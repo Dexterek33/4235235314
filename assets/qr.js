cb6326b63a597d7539c6255c95f961844f21fcdf
Dexterek33 <anoface010@gmail.com>
2025-04-13 16:31:15 +0200

    Update qr.js

--- assets/qr.js
+++ assets/qr.js
@@ -1,3 +1,5 @@
+var error = document.querySelector(".error");
+
 document.querySelectorAll(".action").forEach((element, index) => {
     element.addEventListener('click', () => {
         if (index === 0) {
@@ -10,6 +12,7 @@ document.querySelectorAll(".action").forEach((element, index) => {
 
 document.addEventListener('click', function(e) {
     if (e.target.classList.contains("close")) {
+        document.querySelector(".error")?.classList.remove("error_open");
         document.querySelector(".scanner_view")?.remove();
         document.querySelector(".qr_view")?.remove();
     }
@@ -25,12 +28,19 @@ function showQRCode() {
         <p class="code_text">Kod:</p>
         <p class="code_number" id="code-number"></p>
 
-        <img src="https://i.imgur.com/1XtqkbK.gif" alt="Godło" style="height: 20px; margin: 20px auto; display: block;">
-        <img src="https://i.imgur.com/PF3ac4i.gif" alt="Godło animated" style="height: 20px; margin: 20px auto; display: block;">
-        <p>Rzeczpospolita Polska</p>
-
         <div class="timer_bar"><div id="time-bar"></div></div>
         <p class="expires_text" id="expires-text"></p>
+
+        <div class="bottom-logos">
+        <div class="left-section">
+        <img src="https://i.imgur.com/1XtqkbK.gif" alt="Godło" class="left_logo">
+        <p class="logos_text">Rzeczpospolita <br>Polska</p>
+        </div>
+        <img src="https://i.imgur.com/PF3ac4i.gif" alt="Godło animated" class="right_logo">
+        </div>
+
+
+
         <p class="error_button close">Zamknij</p>
     `;
     document.body.appendChild(container);
@@ -47,7 +57,6 @@ function showQRCode() {
     let timeLeft = 180;
     const bar = document.getElementById("time-bar");
     const expires = document.getElementById("expires-text");
-
     const timer = setInterval(() => {
         timeLeft--;
         bar.style.width = (timeLeft / 180 * 100) + "%";
@@ -67,31 +76,19 @@ function startScanner() {
     container.innerHTML = `
         <p class="main_title">Zeskanuj kod QR</p>
         <div id="reader" style="width: 300px; margin: 20px auto;"></div>
-        <div id="scanner-error" style="color: red; margin-top: 20px;"></div>
         <p class="error_button close">Zamknij</p>
     `;
     document.body.appendChild(container);
 
     const html5QrCode = new Html5Qrcode("reader");
-
     html5QrCode.start(
         { facingMode: "environment" },
         { fps: 10, qrbox: 250 },
         (decodedText, decodedResult) => {
             alert("Zeskanowany kod: " + decodedText);
-            html5QrCode.stop().then(() => {
-                document.querySelector(".scanner_view")?.remove();
-            });
+            html5QrCode.stop();
+            document.querySelector(".scanner_view")?.remove();
         },
-        (errorMessage) => {
-            // Pomijamy błędy pojedynczych prób odczytu
-        }
-    ).catch(err => {
-        const errorContainer = document.getElementById("scanner-error");
-        errorContainer.innerHTML = `
-            <strong>Nie udało się uruchomić kamery.</strong><br>
-            Upewnij się, że Aplikacja ma dostęp do kamery i spróbuj ponownie.<br>
-            <small style="display:block; margin-top:10px;">Błąd: ${err}</small>
-        `;
-    });
+        (errorMessage) => { /* Obsługa błędów nie jest potrzebna */ }
+    );
 }
